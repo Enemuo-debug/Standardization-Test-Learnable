e024c3fcdb632d987e0ff42f8fe67049f1390778
Enemuo-debug <chimzuruoke2017@gmail.com>
2023-03-08 01:57:19 +0100

    Added a pre hook to my User Model to enble me hash the passwwords passed in

--- src/models/User.Model.js
+++ src/models/User.Model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new Schema({
     UserName: {
@@ -16,6 +17,11 @@ const UserSchema = new Schema({
         unique: true
     }
 }, { timestamps: true })
+
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt()
+    this.PassWord = await bcrypt.hash(this.PassWord, salt)
+})
 
 const UserModel = model('Post-it Users', UserSchema)
 
