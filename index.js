8b3b3b59fcd723438dadc76856925830cc70f0fa
Enemuo-debug <chimzuruoke2017@gmail.com>
2023-03-13 00:32:36 +0100

    Created the index.js main file

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const rootRouter = require('./src/routes/index.routers');
+const UserServices = require('./src/services/User.Services');
+const UserController = require('./src/controllers/User.Controller');
+
+const app = express();
+const port = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_DB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+.then(()=>{
+    console.log('Connected to MongoDB')
+    app.listen(port, ()=>{
+        console.log(`Server is running on port ${port}`)
+    })
+}).catch((err)=>{
+    console.log('Error in Connecting to DataBase');
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/', rootRouter);
+app.use(UserController.GetAllUsers)
